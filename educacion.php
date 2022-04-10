dc3bda0c05e2b296ffa971acaa4051758ac15744
AlanZamorano <101685018+AlanZamorano@users.noreply.github.com>
2022-04-09 22:36:58 -0600

    Initial commit

---
+++ educacion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style2.css">
+    <title>CV</title>
+</head>
+
+<body>
+  <?php 
+  include "menu.html";
+  ?>
+      <div class="container">
+          <div class="grid">
+          <div class="escuela1">
+              <img src="morelos.jpg">
+              <h5>Escuela Primaria "Estado de Morelos"</h5>
+              <p>Tecuala, Nayarit. (2007 - 2013)</p>
+          </div>
+          <div class="escuela2">
+              <img src="secundaria.gif">
+              <h5>Escuela Secundaria "Niños Héroes"</h5>
+              <p>Tecuala, Nayarit. (2013 - 2016)</p>
+          </div>
+          <div class="escuela3">
+              <img src="uan.png">
+              <h5>Preparatoria UAN #4</h5>
+              <p>Tecuala, Nayarit. (2016 - 2019)</p>
+          </div>
+          </div>
+          <div class="grid2">
+              <div class="escuela4">
+                  <img src="ut.png">
+                  <h5>Universidad Tecnológica de la Costa</h5>
+                  <p>Santiago Ixcuintla, Nayarit. (2019 - actualidad)</p>
+              </div>
+              <div class="escuela5">
+                  <img src="devf.png">
+                  <h5>DEVF</h5>
+                  <p>Virtual (2022 - actualidad)</p>
+              </div>
+          </div>
+      </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>  
+</body>
+</html>
